9f8163259ae3dc0563023a87e83b1a5d39c691ad
ministr haipa <165479426+ministrxaipa@users.noreply.github.com>
2024-04-07 02:22:16 +1400

    Add files via upload

---
+++ final_win.py
@@ -0,0 +1,33 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import*
+from second_win import*
+class TOOWin (QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUT()
+        self.connects()
+        self.set_appear()
+        self.show()
+    def initUT(self):
+        self.btn_next=QPushButton('txt_next',self)
+        self.xp=QLabel('сюдааа')
+        self.ovr=QLabel('тудааа')
+        self.layout_line=QVBoxLayout()
+        self.layout_line.addWidget(self.xp,alignment=Qt.AlignLeft)
+        self.layout_line.addWidget(self.ovr,alignment=Qt.AlignLeft)
+        self.layout_line.addWidget(self.btn_next,alignment=Qt.AlignLeft)
+        self.setLayout(self.layout_line)
+    def next_click(self):
+        self.tw=TOOWin()
+        self.hide()
+    def connects (self):
+        self.btn_next.clicked.connect(self.next_click)
+    def set_appear(self):
+        win_x,win_y=200,100
+        win_width,win_height=1000,600
+        self.setWindowTitle('txt_title')
+        self.resize(win_width,win_height)
+        self.move (win_x,win_y)
+app=QApplication([])
+mw=TOOWin()
+app.exec()
---
+++ second_win.py
@@ -0,0 +1,56 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import*
+from final_win import*
+class TestWin (QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUT()
+        self.connects()
+        self.set_appear()
+        self.show()
+    def initUT(self):
+        self.btn_next=QPushButton('txt_next',self)
+        self.hello_text=QLabel('Введите Ф.И.О.')
+        self.instruction=QLabel('Полных лет')
+        self.deb=QLineEdit()
+        self.vosrat=QLineEdit()
+        self.info=QLabel('ochen vasno')
+        self.wsd=QPushButton('начать тест',self)
+        self.rap=QLineEdit()
+        self.hu=QLabel('другая важная информация')
+        self.vs=QLineEdit()
+        self.rep=QLabel('дорогие друзья')
+        self.wer=QPushButton('начать финальный тест',self)
+        self.ger=QLineEdit()
+        self.dwe=QLineEdit()
+        self.layout_line=QVBoxLayout()
+        self.layout_line.addWidget(self.hello_text,alignment=Qt.AlignLeft)
+        self.layout_line.addWidget(self.deb,alignment=Qt.AlignLeft)
+        self.layout_line.addWidget(self.instruction,alignment=Qt.AlignLeft)
+        self.layout_line.addWidget(self.vosrat,alignment=Qt.AlignLeft)
+        self.layout_line.addWidget(self.info,alignment=Qt.AlignLeft)
+        self.layout_line.addWidget(self.wsd, alignment=Qt.AlignLeft)
+        self.layout_line.addWidget(self.rap,alignment=Qt.AlignLeft )
+        self.layout_line.addWidget(self.hu, alignment=Qt.AlignLeft)
+        self.layout_line.addWidget(self.vs,alignment=Qt.AlignLeft)
+        self.layout_line.addWidget(self.rep,alignment=Qt.AlignLeft)
+        self.layout_line.addWidget(self.wer, alignment=Qt.AlignLeft)
+        self.layout_line.addWidget(self.btn_next,alignment=Qt.AlignLeft)
+        self.layout_line.addWidget(self.ger, alignment=Qt.AlignLeft)
+        self.layout_line.addWidget(self.dwe, alignment=Qt.AlignLeft)
+        self.setLayout(self.layout_line)
+        self.layout_line.addWidget(self.btn_next,alignment=Qt.AlignLeft)
+    def next_click(self):
+        self.tw=TOOWin()
+        self.hide()
+    def connects (self):
+        self.btn_next.clicked.connect(self.next_click)
+    def set_appear(self):
+        win_x,win_y=200,100
+        win_width,win_height=1000,600
+        self.setWindowTitle('txt_title')
+        self.resize(win_width,win_height)
+        self.move (win_x,win_y)
+app=QApplication([])
+mw=TestWin()
+app.exec()
